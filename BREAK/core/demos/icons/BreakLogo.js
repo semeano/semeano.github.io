7c33d52ffc111535db16807a110df8acb76a1e95
Pedro Semeano <semeano@gmail.com>
2014-07-11 19:22:12 +0100

    BREAK project added

---
+++ BREAK/core/demos/icons/BreakLogo.js
@@ -0,0 +1,105 @@
+function BreakLogo(pos){
+
+  this.pos = new Vector(pos.x, pos.y);
+
+  this.canvas = [
+
+    // R
+    new pixel(this.pos, -15, -3),
+    new pixel(this.pos, -14, -3),
+    new pixel(this.pos, -13, -3),
+    new pixel(this.pos, -12, -3),
+    new pixel(this.pos, -11, -3),
+    new pixel(this.pos, -10, -3),
+    new pixel(this.pos, -9, -3),
+
+    new pixel(this.pos, -15, -2),
+    new pixel(this.pos, -14, -2),
+    new pixel(this.pos, -13, -2),
+    new pixel(this.pos, -12, -2),
+    new pixel(this.pos, -11, -2),
+    new pixel(this.pos, -10, -2),
+    new pixel(this.pos, -9, -2),
+    new pixel(this.pos, -8, -2),
+
+    new pixel(this.pos, -15, -1),
+    new pixel(this.pos, -14, -1),
+    new pixel(this.pos, -13, -1),
+    new pixel(this.pos, -12, -1),
+    new pixel(this.pos, -11, -1),
+    new pixel(this.pos, -10, -1),
+    new pixel(this.pos, -9, -1),
+    new pixel(this.pos, -8, -1),
+
+    new pixel(this.pos, -15, 0),
+    new pixel(this.pos, -14, 0),
+    new pixel(this.pos, -13, 0),
+    new pixel(this.pos, -12, 0),
+    new pixel(this.pos, -11, 0),
+    new pixel(this.pos, -10, 0),
+    new pixel(this.pos, -9, 0),
+
+    new pixel(this.pos, -15, 1),
+    new pixel(this.pos, -14, 1),
+    new pixel(this.pos, -13, 1),
+    new pixel(this.pos, -12, 1),
+    new pixel(this.pos, -11, 1),
+    new pixel(this.pos, -10, 1),
+    new pixel(this.pos, -9, 1),
+
+    // E
+    new pixel(this.pos, -3.6, -4),
+    new pixel(this.pos, -3, -4),
+    new pixel(this.pos, 1, -4),
+    new pixel(this.pos, 2, -4),
+    new pixel(this.pos, 3, -4),
+
+    new pixel(this.pos, -3.6, -3),
+    new pixel(this.pos, -3, -3),
+    new pixel(this.pos, -2, -3),
+    new pixel(this.pos, -1, -3),
+    new pixel(this.pos, 0, -3),
+    new pixel(this.pos, 1, -3),
+    new pixel(this.pos, 2, -3),
+
+    new pixel(this.pos, -3.6, -2),
+    new pixel(this.pos, -3, -2),
+    new pixel(this.pos, -2, -2),
+
+    new pixel(this.pos, -3.6, -1),
+    new pixel(this.pos, -3, -1),
+    new pixel(this.pos, -2, -1),
+    new pixel(this.pos, -1, -1),
+    new pixel(this.pos, 0, -1),
+    new pixel(this.pos, 1, -1),
+    new pixel(this.pos, 2, -1),
+
+    new pixel(this.pos, -3.6, 0.4),
+    new pixel(this.pos, -3, 0.4),
+    new pixel(this.pos, -2, 0.4),
+    new pixel(this.pos, -1, 0.4),
+    new pixel(this.pos, 0, 0.4),
+    new pixel(this.pos, 1, 0.4),
+    new pixel(this.pos, 2, 0.4),
+
+    new pixel(this.pos, -3.6, 1),
+    new pixel(this.pos, -3, 1),
+    new pixel(this.pos, -2, 1),
+
+    new pixel(this.pos, -3.6, 2),
+    new pixel(this.pos, -3, 2),
+
+    new pixel(this.pos, -3.6, 3),
+    new pixel(this.pos, -3, 3),
+
+    new pixel(this.pos, -3.6, 4.5),
+    new pixel(this.pos, -3, 4.5),
+    new pixel(this.pos, -2, 4.5),
+    new pixel(this.pos, -1, 4.5),
+    new pixel(this.pos, 0, 4.5),
+    new pixel(this.pos, 1, 4.5),
+    new pixel(this.pos, 2, 4.5),
+    new pixel(this.pos, 3, 4.5),
+
+  ];
+}
